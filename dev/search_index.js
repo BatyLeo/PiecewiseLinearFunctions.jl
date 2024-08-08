f04139d4b1139b1fd4ffb7d2eb2ca65c0ac927f9
Documenter.jl <documenter@juliadocs.github.io>
2024-08-08 15:23:05 +0000

    build based on 950d739

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PiecewiseLinearFunctions","category":"page"},{"location":"#PiecewiseLinearFunctions","page":"Home","title":"PiecewiseLinearFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PiecewiseLinearFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PiecewiseLinearFunctions]","category":"page"}]
+}
