107d64bfc03ba2a58275570a24a891040416da93
BatyLeo <leo.baty67@gmail.com>
2024-08-13 16:06:46 +0200

    update Plots extension

--- ext/PiecewiseLinearFunctionsPlotExt.jl
+++ ext/PiecewiseLinearFunctionsPlotExt.jl
@@ -9,8 +9,10 @@ $TYPEDSIGNATURES
 Plot input `PiecewiseLinearFunction` `f` with an offset `δ` on the x-axis on both sides of the breakpoints.
 """
 @recipe function Plots.plot(f::PiecewiseLinearFunction, δ=1.0)
-    X = vcat(f.x[1] - δ, f.x, f.x[end] + δ)
-    Y = f.(X)
+    x0 = f.x[1] - δ
+    xinf = f.x[end] + δ
+    X = vcat(x0, f.x, xinf)
+    Y = vcat(f(x0), f.y, f(xinf))
     return X, Y
 end
 end
